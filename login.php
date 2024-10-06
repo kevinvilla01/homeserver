02baeffefc1823397828c2fdceb08b21d7f3fb6f
kevinvilla01 <kevin01villa0101@gmail.com>
2024-10-05 23:55:51 -0600

    adicion de dependencias

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- BOOTSTRAP -->
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <script src="bootstrap.bundle.min.js"></script>
+    <!-- JQUERY -->
+    <script src="jquery-3.7.1.min.js"></script>
+    <!-- sweetalert -->
+    <script src="sweetalert2.all.min.js"></script>
+    <title>VS - Login</title>
+</head>
+<body>
+    
+</body>
+</html>
