9d50a71e890fad3c36be3c659cca11660aa8f45f
kevinvilla01 <kevin01villa0101@gmail.com>
2024-10-06 14:24:50 -0600

    validacion de login

--- php/valid_login.php
+++ php/valid_login.php
@@ -3,5 +3,34 @@ require_once 'db_connect.php';
 
 $connection  = db_connect();
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    //Prevenir SQL INJECTION conn consultas separados
+    $query = "SELECT * FROM user WHERE username = $1";
+    $result = pg_query_params($connection, $query, array($username));
+
+    if ($result && pg_num_rows($result) > 0) {
+        $user = pg_fetch_assoc($result);
+
+        //Verificar contraseña
+        if (password_verify($password, $user['password'])) {
+            session_start();
+            $_SESSION['logged_in'] = true;
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['username'] = $user['username'];
+
+            //Redireccionar al home
+            header('Location: home.php');
+            exit;
+        } else {
+            echo 'Incorrect password';
+        }
+    } else {
+        echo 'User not found';
+    }
+}
+pg_close($connection);
 
 ?>
