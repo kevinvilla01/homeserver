390624845773be2d05c8907a457ad0f39f546090
kevinvilla01 <kevin01villa0101@gmail.com>
2024-10-06 00:13:02 -0600

    login page

--- login.php
+++ login.php
@@ -13,6 +13,27 @@
     <title>VS - Login</title>
 </head>
 <body>
-    
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-4">
+                <div class="login-container">
+                    <h2>Login</h2>
+                    <form id="loginForm" action="../php/valid_login.php" method="POST">
+                        <div class="mb-3">
+                            <label for="username" class="form-label">Username</label>
+                            <input type="text" class="form-control" id="username" name="username" required>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="password" class="form-label">Password</label>
+                            <input type="password" class="form-control" id="password" name="password" required>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Login</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
