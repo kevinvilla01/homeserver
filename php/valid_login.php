0f0920a69e6e46ca1146ed51b5458c9ecf05a0e1
kevinvilla01 <kevin01villa0101@gmail.com>
2024-10-06 14:49:49 -0600

    aver si jala

--- php/valid_login.php
+++ php/valid_login.php
@@ -3,21 +3,15 @@ require_once 'db_connect.php';
 
 $connection  = db_connect();
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
     //Prevenir SQL INJECTION conn consultas separados
-    $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
-    $result = pg_query_params($connection, $query, array($username));
+    $query = pg_query($connection, "SELECT * FROM users WHERE username = '$username' AND password = '$password'");
 
-    if ($result && pg_num_rows($result) > 0) {
-        $user = pg_fetch_assoc($result);
+    if (pg_num_rows($query) > 0) {
+        $user = pg_fetch_assoc($query);
 
             //Redireccionar al home
             header('Location: ../home.php');
@@ -28,6 +22,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     } else {
         echo 'User not found';
     }
+
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
 pg_close($connection);
 
 ?>
